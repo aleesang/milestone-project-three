1cee08ec086e34c60d00b052ba3074f44277b7c0
aleesang <aleesang20@gmail.com>
2020-05-21 09:49:36 +0000

    initialising project

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:r00tUser@cluster0-aveek.mongodb.net/Music_Library?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
