6abedef31ccf447bc51744fb9e9296b3a33840cd
aleesang <aleesang20@gmail.com>
2020-06-13 23:27:51 +0930

    amended app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
