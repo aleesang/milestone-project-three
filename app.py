ffc5b8291dcd317ba0478f4684f9ed2022f3ab34
aleesang <aleesang20@gmail.com>
2020-06-26 00:07:05 +0930

    added comments to all template files and amended CSS

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mongo = PyMongo(app)
 # for sharing on socials on view song page
 share.init_app(app)
 
-# routes to home page
+# routes to home page where all genres are displayed
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -95,12 +95,12 @@ def get_songs():
                            songs=mongo.db.songs.find().sort("_id", -1))
                            
                            
-# routes to individual song info
+# routes to view song info
 @app.route("/show_song/<song_id>")
 def show_song(song_id):
-    the_song = mongo.db.songs.find_one({"_id": ObjectId(song_id)})
-    all_genres = mongo.db.genre.find()
-    return render_template('showsong.html', song=the_song, genres=all_genres)
+        the_song = mongo.db.songs.find_one({"_id": ObjectId(song_id)})
+        all_genres = mongo.db.genre.find()
+        return render_template('showsong.html', song=the_song, genres=all_genres)
     
 
 # page to add a new song
@@ -112,7 +112,7 @@ def add_song():
 
 
 # inserts new song
-@app.route('/insert_song', methods=['POST'])
+@app.route('/insert_song', methods=['GET', 'POST'])
 def insert_song():
     songs =  mongo.db.songs
     songs.insert_one(request.form.to_dict())
@@ -120,7 +120,7 @@ def insert_song():
 
 
 # page to edit song
-@app.route('/edit_song/<song_id>', methods=['POST'])
+@app.route('/edit_song/<song_id>', methods=['GET', 'POST'])
 def edit_song(song_id):
     the_song =  mongo.db.songs.find_one({"_id": ObjectId(song_id)})
     all_genres =  mongo.db.genre.find()
@@ -128,7 +128,7 @@ def edit_song(song_id):
 
 
 # updates song after user saves changes from edit and redirects to "all songs" page
-@app.route('/update_song/<song_id>', methods=['POST'])
+@app.route('/update_song/<song_id>', methods=['GET', 'POST'])
 def update_song(song_id):
     songs = mongo.db.songs
     songs.update( 
@@ -141,7 +141,7 @@ def update_song(song_id):
         'artist_name':request.form.get('artist_name'),
         'song_link':request.form.get('song_link'),
     })
-    return redirect(url_for('get_songs'))
+    return redirect(url_for('show_song'))
 
 
 # removes song after user clicks delete
